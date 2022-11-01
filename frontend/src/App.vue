6353e9185fc23d3893da0045c5ad29afaae4591d
fs200817-gotou-kei <k_goto@freestyles.jp>
2022-11-01 21:38:12 +0900

    app css setting and header css setting

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div></div>
+  <AppHeader />
+  <EstimatesSearch />
 </template>
 
 <script>
+import EstimatesSearch from './components/EstimatesSearch.vue'
+import AppHeader from './components/AppHeader.vue'
+import './css/app.css'
 
 export default {
   name: 'App',
+  components: {
+    AppHeader,
+    EstimatesSearch
+  }
 }
 </script>
---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="app-header">
+        <div class="content">
+            <h1 class="header-title">販売管理システム</h1>
+            <span class="sub-title">SALESWORKER</span>
+            <nav class="header-nav">
+                <ul class="nav-list">
+                    <li class="nav-item">見積検索</li>
+                    <li class="nav-item">見積登録</li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+import '../css/header.css'
+
+export default {
+    name: 'AppHeader'
+}
+</script>
