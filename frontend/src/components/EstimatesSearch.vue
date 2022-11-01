9045123ee88d50c11bbe3e475601d9e5315c7f83
fs200817-gotou-kei <k_goto@freestyles.jp>
2022-11-02 07:16:33 +0900

    complete in a general way

--- frontend/src/components/EstimatesSearch.vue
+++ frontend/src/components/EstimatesSearch.vue
@@ -1,36 +1,40 @@
 <template>
-    <div class="commom-width">
+    <div class="content">
         <div>
-            <h1>見積検索</h1>
+            <h1 class="page-title">見積検索</h1>
             <span>検索条件に部分一致した見積情報が結果に表示されます。</span>
         </div>
+        <p class="border">検索条件</p>
         <div class="search-conditions">
-            <p class="border">検索条件</p>
-            <div class="estimate-condtion">
+            <div class="estimate-condition">
                 <label class="search-label">見積番号</label>
                 <input class="search-input" />
                 <button class="search-button">検索</button>
             </div>
-            <div class="estimate-condtion">
+            <div class="estimate-condition">
                 <label class="search-label">案件名</label>
                 <input class="search-input" />
                 <button class="search-button">検索</button>
             </div>
-            <div class="estimate-condtion">
+            <div class="estimate-condition">
                 <label class="search-label">見積ステータス</label>
-                <input class="search-input" />
+                <select v-model="this.selectEstimate">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                </select>
                 <button class="search-button">検索</button>
             </div>
-            <div class="estimate-condtion">
+            <div class=" estimate-condition">
                 <label class="search-label">顧客名</label>
-                <input class="search-input" />
+                <input class="search-input" readonly />
                 <button class="customer_button">顧客選択</button>
                 <button class="clear-button">クリア</button>
                 <button class="search-button">検索</button>
             </div>
-            <div class="estimate-condtion">
+            <div class="estimate-condition">
                 <label class="search-label">担当者名</label>
-                <input class="search-input" />
+                <input class="search-input" readonly />
                 <button class="customer_button">顧客選択</button>
                 <button class="clear-button">クリア</button>
                 <button class="search-button">検索</button>
@@ -40,10 +44,17 @@
 </template>
 
 <script>
+import '../css/estimates_search.css'
+import '../css/input_common.css'
 import EstimatesService from '../services/EstimatesService'
 
 export default {
     name: 'EstimatesSearch',
+    data() {
+        return {
+            selectEstimate: null
+        }
+    },
     mounted() {
         EstimatesService.getAllEstimates()
     }
